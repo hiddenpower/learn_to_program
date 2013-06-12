ac9ce04204f86267e1c434fd86eb27564e42507b
Gustavo Sandoval <gusxsandoval@gmail.com>
2013-06-12 17:12:43 -0500

    chapter 5

--- Chapter2.rb
+++ Chapter2.rb
@@ -1,30 +1,19 @@
-def hours_in_year
+puts "hours in year"
   #1 year = 365 days
   #1 day = 24 hours
-  result = 365 * 24
-end
+  puts 365 * 24
 
-#hours_in_year
-
-def minutes_in_decade
+puts "minutes in decade"
   #1 decade = 10 years
   #1 year = 8760 hours
   #1 hours = 60 minutes
-  result = 10*hours_in_year*60
-end
+  puts 10*365*24*60
 
-#minutes_in_decade
-
-def age_in_seconds(age)
+puts "age in seconds"
   #1 year = 8760 hours
   #1 hours = 60 minutes
   #1 minute = 60 seconds
-  result = age*hours_in_year*60*60
-end
-#age_in_seconds(25)
+  puts (25 * 365 * 24 * 60 * 60)
 
-def authors_age
-  result = (1111000000)/(3600*8760)
-end
-
-#authors_age
+puts "authors age"
+  puts (1111000000)/(3600*8760)
---
+++ Chapter5.rb
@@ -0,0 +1,8 @@
+puts 'Hey dude! What\'s your first name?'
+nameF = gets.chomp
+puts 'And your middle name?'
+nameM = gets.chomp
+puts 'And... your last name?'
+nameL = gets.chomp
+puts ''
+puts 'Nice meeting you ' + nameF + ' ' + nameM + ' ' + nameL + '! :D'
